3ee71e884b34a8b2bbd1e5dc1a6a6f5d4186cfcd
Kavindu Fernando <kavindufernando329@gmail.com>
2024-03-20 17:10:14 +0530

    PRINT ALL ELEMENT BST - JAVA

--- Tree/JAVA/Binary_Search_Tree/_3_Print_All_Elements/src/Main.java
+++ Tree/JAVA/Binary_Search_Tree/_3_Print_All_Elements/src/Main.java
@@ -9,71 +9,61 @@ class treeNode {
 
 class BST {
 
-    // create a new node
-    private treeNode createNewNode(int k) {
-        treeNode a = new treeNode();
-        a.data = k;
-        a.left = null;
-        a.right = null;
-        return a;
+    private treeNode createNewNode(int data){
+        treeNode node = new treeNode();
+        node.data = data;
+        node.left = null;
+        node.right = null;
+        return node;
     }
-
-    // insert a new node
-    public treeNode insert(treeNode root, int data) {
-        if( root == null) {
-          return createNewNode(data);
+    public treeNode insert(treeNode root, int data){
+        if (root == null){
+            return createNewNode(data);
         }
 
-        if(data < root.data){
-            root.left = insert(root.left,data);
+        if (data < root.data){
+            root.left = insert(root.left, data);
+        } else if (data > root.data) {
+            root.right = insert(root.right, data);
         }
-        else if(data > root.data){
-            root.right = insert(root.right,data);
-        }
+
         return root;
     }
 
-    // delete a node
-    public treeNode deleteNode(treeNode root, int value) {
-        if(root == null) {
+    public treeNode deleteNode(treeNode root, int data){
+        if (root == null){
             return null;
         }
 
-        // find the node to be deleted
-        if(value < root.data) {
-            root.left = deleteNode(root.left, value);
-        }
-        else if(value > root.data){
-            root.right = deleteNode(root.right, value);
-        }
-        else{
-            // if the node has one child or no child
-            if(root.left == null){
+        if (data < root.data){
+            root.left = deleteNode(root.left, data);
+        } else if (data > root.data) {
+            root.right = deleteNode(root.right, data);
+        }else{
+
+            if (root.left == null){
                 return root.right;
-            }
-            else if(root.right == null){
+            } else if (root.right == null) {
                 return root.left;
             }
 
-            // if the node has two children
             root.data = minValue(root.right);
-            root.right = deleteNode(root.right, root.data);
+            root.right = deleteNode(root.right,root.data);
+
         }
         return root;
     }
 
-    // find the minimum value of a branch
     private int minValue(treeNode root){
         int minValue = root.data;
         while(root.left != null){
             root = root.left;
-            minValue = root.left.data;
+            minValue = root.data;
         }
         return minValue;
     }
 
 
-    // draw the tree BONUS
 
     public void draw(treeNode root) {
         JFrame frame = new JFrame("Binary Search Tree");
@@ -116,13 +106,12 @@ class BST {
         }
     }
 }
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
         treeNode root = null;
-
         BST a = new BST();
 
         root = a.insert(root,10);
@@ -142,11 +131,29 @@ public class Main {
         root = a.insert(root,999);
         root = a.insert(root,11);
         root = a.insert(root,3);
-
-        root = a.deleteNode(root, 20);
         a.draw(root);
-        root = a.deleteNode(root, 34);
-//        a.draw(root);
 
+        treeNode root1 = null;
+        BST b = new BST();
+        root1 = b.insert(root1,10);
+        root1 = b.insert(root1,20);
+        root1 = b.insert(root1,5);
+        root1 = b.insert(root1,6);
+        root1 = b.insert(root1,4);
+        root1 = b.insert(root1,15);
+        root1 = b.insert(root1,25);
+        root1 = b.insert(root1,35);
+        root1 = b.insert(root1,1);
+        root1 = b.insert(root1,2);
+        root1 = b.insert(root1,21);
+        root1 = b.insert(root1,34);
+        root1 = b.insert(root1,22);
+        root1 = b.insert(root1,0);
+        root1 = b.insert(root1,999);
+        root1 = b.insert(root1,11);
+        root1 = b.insert(root1,3);
+
+        root1 = b.deleteNode(root1, 20);
+        b.draw(root1);
     }
 }
