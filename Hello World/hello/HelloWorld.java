dddbdd67498ea20e78b0edb537d2e9fe0f3d2212
Kavindu Fernando <kavindufernando329@gmail.com>
2024-04-24 02:40:49 +0530

    hello world

---
+++ Hello World/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+/* *****************************************************************************
+ *  Name:              Kavindu Fernando
+ *  Last modified:     April 24, 2024
+ **************************************************************************** */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
